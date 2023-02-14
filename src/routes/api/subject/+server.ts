71a3da8da61f22455decaf4afa4b57da82c6cfe5
Gus Ramírez <gustavo@aplazo.mx>
2023-02-13 23:01:29 -0600

    fix: remove subject server file
    - remove temporarily subject server file to allow complete build

--- src/routes/api/subject/+server.ts
+++
@@ -1,6 +0,0 @@
-import { env } from '$env/dynamic/private';
-import { postMethodFactory } from '$lib/factories/post-create-server';
-
-const baseUrl = env.API_BASE_URL || 'http://localhost:3000';
-
-export const POST = postMethodFactory(`${baseUrl}/api/v1/subject`);
