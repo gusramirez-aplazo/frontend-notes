92fbdd4debdcb6d3d1cd6cd07a8cb8f9598fc402
Gus Ramírez <gustavo@aplazo.mx>
2023-02-25 22:50:25 -0600

    feat: add cornell component

---
+++ src/lib/modules/corrnell/components/cornell.svelte
@@ -0,0 +1,54 @@
+<script lang="ts">
+	import { Button, TabItem, Tabs, Textarea } from 'flowbite-svelte';
+
+	const contentConfig = {
+		id: 'content',
+		name: 'content',
+		label: 'Your content',
+		rows: 3,
+		placeholder: 'Take your notes...'
+	};
+	const cuesConfig = {
+		id: 'cues',
+		name: 'cues',
+		label: 'Your cues',
+		rows: 3,
+		placeholder: 'Do you have any cues?'
+	};
+
+	function createOne(this: HTMLFormElement) {
+		console.log(this);
+	}
+</script>
+
+<form class="grid grid-cols-1 gap-5" on:submit|preventDefault={createOne}>
+	<label for="title" class="flex bg-transparent w-full justify-center">
+		<input
+			class="outline-none
+				focus:ring-transparent
+				border-none
+				bg-transparent
+				py-2
+    "
+			type="text"
+			placeholder="Awesome title"
+			id="title"
+			name="title"
+		/>
+	</label>
+
+	<div>
+		<Tabs style="full">
+			<TabItem open title="Content">
+				<Textarea {...contentConfig} />
+			</TabItem>
+			<TabItem title="Cues">
+				<Textarea {...cuesConfig} />
+			</TabItem>
+		</Tabs>
+	</div>
+
+	<div class="flex justify-end w-full">
+		<Button type="submit">Save Note</Button>
+	</div>
+</form>
--- src/routes/note/+page.svelte
+++ src/routes/note/+page.svelte
@@ -1,11 +1,15 @@
 <script lang="ts">
 	import { Card } from 'flowbite-svelte';
-	import type { PageData } from './$types';
+
 	import { notifierService } from '$lib/modules/notifier/services/tostr-store.service';
-	import Subject from '$lib/modules/subject/components/subject.svelte';
-	import Category from '$lib/modules/category/components/category.svelte';
 	import { subjectStoreService } from '$lib/modules/subject/services/subject-store.service';
 	import { categoryStoreService } from '$lib/modules/category/services/category-store.service';
+
+	import Subject from '$lib/modules/subject/components/subject.svelte';
+	import Category from '$lib/modules/category/components/category.svelte';
+	import Cornell from '$lib/modules/corrnell/components/cornell.svelte';
+
+	import type { PageData } from './$types';
 
 	export let data: PageData;
 
@@ -28,5 +32,6 @@
 	<Card padding="md" size="md">
 		<Category />
 		<Subject />
+		<Cornell />
 	</Card>
 </main>
